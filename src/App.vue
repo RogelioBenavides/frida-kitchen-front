2098235606296e2bf167f0c6accc4bdcc433ae9f
Rogelio Benavides <roger.benavidesdelarosa@gmail.com>
2023-11-11 02:04:56 -0600

    NavBar Completed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
-  <SignIn></SignIn>
+  <HomePage></HomePage>
 </template>
 
 <script>
 // import LogIn from './components/LogIn.vue';
-import SignIn from './components/SignIn.vue';
+// import SignIn from './components/SignIn.vue';
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
     // LogIn,
-    SignIn
+    HomePage,
   }
 }
 </script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <NavBar></NavBar>
+    </div>
+</template>
+<script>
+import NavBar from './NavBar.vue';
+
+export default {
+    name: 'HomePage',
+    components: {
+        NavBar,
+  }
+}
+</script>
+<style>
+    
+</style>
