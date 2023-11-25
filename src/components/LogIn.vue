2cc6ea758f3750a529aed31895c225075f798814
Josemgallegos <126370753+Josemgallegos@users.noreply.github.com>
2023-11-25 17:04:25 -0600

    change login and signin style

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -1,51 +1,40 @@
 <template>
-    <img src="../assets/images/frida-logo.png" alt="logo" class="head-logo" />
-    <div class="card">
-        <form @submit.prevent="onFinish">
-            <div class="mb-3">
-                <label class="form-label">Correo Electrónico</label>
-                <input v-model="username" type="email" class="form-control" placeholder="ejemplo@correo.com" required>
+    <div style="height: 100vh;">
+        <div class="container d-flex flex-column justify-content-center align-items-center h-100 gap-4">
+            <img src="../assets/images/frida-logo.png" alt="logo" class="head-logo" style="width: 16rem;" />
+            <div class="card w-50 p-3">
+                <form @submit.prevent="onFinish">
+                    <div class="mb-3">
+                        <label class="form-label">Correo Electrónico</label>
+                        <input v-model="username" type="email" class="form-control" placeholder="ejemplo@correo.com" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                        <input v-model="password" type="password" class="form-control" placeholder="1234" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                <input v-model="password" type="password" class="form-control" placeholder="1234" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
     </div>
 </template>
-<script>
 
-export default {
-    data() {
-        return {
-            username: '',
-            password: '',
-        }
-    },
-    methods: {
-        onFinish() {
-            alert(this.username + this.password + 'Bienvenido')
+<script>
+    export default {
+        data() {
+            return {
+                username: '',
+                password: '',
+            }
+        },
+        methods: {
+            onFinish() {
+                alert(this.username + this.password + 'Bienvenido')
+            }
         }
     }
-}
 </script>
 
 <style>
-body {
-    font-family: 'Roboto', sans-serif;
-    background-color: '#F7F3EB';
-}
-.head-logo {
-    width: 15%;
-    display: block;
-    margin: 0 auto;
-    margin-top: 5%;
-}
-.card {
-    width: 30%;
-    padding: 20px;
-    margin: 0 auto;
-    margin-top: 50px;
-}
+
 </style>
--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,23 +1,28 @@
 <template>
-    <img src="../assets/images/frida-logo.png" alt="logo" class="head-logo" />
-    <div class="card">
-        <form @submit.prevent="onFinish">
-            <div class="mb-3">
-                <label class="form-label">Correo Electrónico</label>
-                <input v-model="username" type="email" class="form-control" placeholder="ejemplo@correo.com" required>
+    <div style="height: 100vh;">
+        <div class="container d-flex flex-column justify-content-center align-items-center h-100 gap-4">
+            <img src="../assets/images/frida-logo.png" alt="logo" class="head-logo" style="width: 16rem;" />
+            <div class="card w-50 p-3">
+                <form @submit.prevent="onFinish">
+                    <div class="mb-3">
+                        <label class="form-label">Correo Electrónico</label>
+                        <input v-model="username" type="email" class="form-control" placeholder="ejemplo@correo.com" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                        <input v-model="firstPassword" type="password" class="form-control" placeholder="1234" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                        <input v-model="secondPassword" type="password" class="form-control" placeholder="1234" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                <input v-model="firstPassword" type="password" class="form-control" placeholder="1234" required>
-            </div>
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                <input v-model="secondPassword" type="password" class="form-control" placeholder="1234" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
     </div>
 </template>
+
 <script>
 
 export default {
@@ -40,21 +45,6 @@ export default {
 }
 </script>
 
-<style>
-body {
-    font-family: 'Roboto', sans-serif;
-    background-color: '#F7F3EB';
-}
-.head-logo {
-    width: 15%;
-    display: block;
-    margin: 0 auto;
-    margin-top: 5%;
-}
-.card {
-    width: 30%;
-    padding: 20px;
-    margin: 0 auto;
-    margin-top: 50px;
-}
+<style scoped>
+
 </style>
