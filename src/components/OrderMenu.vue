0187ccc49bfcb91533971217a88f2621d81c474f
Rogelio Benavides <roger.benavidesdelarosa@gmail.com>
2023-11-29 03:26:05 -0600

    fixed menu ip

--- src/components/OrderMenu.vue
+++ src/components/OrderMenu.vue
@@ -44,7 +44,7 @@ export default {
     },
     methods: {
         loadFavorites() {
-            fetch("35.209.86.116:5000/meals/json")
+            fetch("http://35.209.86.116:5000/meals/json")
                 .then((response) => response.json())
                 .then((data) => (this.meals = data))
                 .then(() => console.log(this.meals))
