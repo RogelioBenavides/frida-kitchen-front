6f8eed7edbdfabef2ff751f893ad062b4c529b71
Josemgallegos <126370753+Josemgallegos@users.noreply.github.com>
2023-11-29 23:08:06 -0600

    update tables

--- src/components/SupervisorTables.vue
+++ src/components/SupervisorTables.vue
@@ -1,15 +1,15 @@
 <template>
     <NavBar></NavBar>
     <div class="row">
-        <div class="col-6">
+        <div class="col-6 ps-5">
             <h1 class="text-center py-5">Camiones</h1>
             <HighchartOne></HighchartOne>
             <HighchartTwo></HighchartTwo>
         </div>
-        <div class="col-6">
+        <div class="col-6 pe-5" style="height: 90vh; overflow-y: scroll;">
             <h1 class="text-center py-5">Órdenes</h1>
-            <div v-for="(order, index) in orders" :key="index">
-                <div class="border rounded shadow p-5 d-flex flex-column mb-5" v-for="o in order" :key="o.id">
+            <div v-for="(order, index) in orders" :key="index" class="d-flex flex-column-reverse">
+                <div class="border rounded shadow p-5 d-flex flex-column mb-5" style="background-color: white;" v-for="o in order" :key="o.id">
                     <h6>Número de Orden: {{ o.id }}</h6>
                     <div class="row row-cols-2">
                         <div class="col py-4 px-3" v-for="(meal, index) in o.meals" :key="index">
